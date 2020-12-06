15fa56db846b0597ceddf24df01e521c1964528c
Kelvin Makamu <kmakamu@safaricom.co.ke>
2020-12-06 20:02:25 +0300

    Adds Encoding Test class skeleton

---
+++ src/test/java/models/EncodingTest.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+/**
+ *
+ * @author KMAKAMU
+ */
+public class EncodingTest {
+    
+    public EncodingTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Test
+    public void Encoding_InstantiatingClass(){
+        
+    }
+    
+}
